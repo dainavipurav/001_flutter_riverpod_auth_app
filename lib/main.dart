602bd3a59e8ed08ca732cdff20b51c200c86d466
dainavi.purav <dainavi.purav@cmss.in>
2024-09-16 02:58:44 +0530

    feat : Firebse project implemented and firebase hosting enabled and hosted the first web app to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'models/user.dart';
 import 'pages/register/register.dart';
 
 final userProvider = StateNotifierProvider<UserNotifier, User?>(
   (ref) => UserNotifier(null),
 );
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MainApp()));
 }
 
